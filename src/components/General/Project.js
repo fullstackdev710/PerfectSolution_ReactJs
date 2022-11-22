1bafd82755cd0908a6f9ec31246b5f6043c6047d
fullstackdev710 <fullstack710dev@gmail.com>
2022-11-22 23:16:13 +0800

    update project componant

--- src/components/General/Project.js
+++ src/components/General/Project.js
@@ -7,10 +7,40 @@ const Project = () => {
    const {id} = useParams();
    const project = projects[id];
 
-   if (!project) {
+   const ProjectNoExist = () => {
       return(
-         <div id="test">
+         <div className="container z-1" style={{ position: 'inherit' }}>
             This project doesn't exist!
+         </div>
+      );
+   }
+
+   const ProjectDetail = () => {
+      return(
+         <div className="container z-1" style={{ position: 'inherit' }}>
+            <Col>
+               <Row>
+                  <Col span={24}>
+                     <h1 className="text-white text-center">{project.title}</h1>
+                  </Col>
+               </Row>
+               <section className="pt-5 pb-5">
+                  <div className="container">
+                     <Row gutter={24} >
+                        <Col {...span}>
+                           <p><strong>Company Name: </strong>{project.company}</p>
+                           <p><strong>Duration: </strong>{project.duration}</p>
+                           <p><strong>Website: </strong>
+                              <a href={project.website}>{project.website}</a>
+                           </p>
+                           <p><strong>Framework: </strong>{project.framework}</p>
+                           <p><strong>Template: </strong>{project.template}</p>
+                           <p><strong>Details: </strong>{project.details}</p>
+                        </Col>
+                     </Row>
+                  </div>
+               </section>
+            </Col>
          </div>
       );
    }
@@ -19,64 +49,24 @@ const Project = () => {
       lg: 12,
       md: 12,
       sm: 24
-  }
+   }
    
    return(
-      <Row style={{ padding: "250px 0 150px 0", backgroundColor: "#000000" }}>
-            <Col span={24} className="container">
-                <Row>
-                    <div className="background-overlay">
-                        <ParticlesComponent />
-                    </div>
-                </Row>
-                <Row>
-                    <div className="container z-1" style={{ position: 'inherit' }}>
-                        <Col>
-                            <Row>
-                                <Col span={24}>
-                                    <h1 className="text-white text-center">{project.title}</h1>
-                                </Col>
-                            </Row>
-                            <section className="pt-5 pb-5">
-                                <div className="container">
-                                    <Row gutter={24} >
-                                        <Col {...span}>
-                                          <p><strong>Company Name: </strong>{project.company}</p>
-                                          <p><strong>Duration: </strong>{project.duration}</p>
-                                          <p><strong>Website: </strong>
-                                          <a href={project.website}>{project.website}</a>
-                                          </p>
-                                          <p><strong>Framework: </strong>{project.framework}</p>
-                                          <p><strong>Template: </strong>{project.template}</p>
-                                          <p><strong>Details: </strong>{project.details}</p>
-                                        </Col>
-                                    </Row>
-                                </div>
-                            </section>
-                        </Col>
-                    </div>
-                </Row>
-            </Col>
-        </Row>
-      // <div className="my-3">
-      //    <Row gutter={16} style={{ width: "90%", maxWidth: "640px", margin: "auto" }}>
-      //       <Col {...spanM}>
-      //          <h5 className="text-white">{project.title}</h5>
-      //          <p><strong>Company Name: </strong>{project.company}</p>
-      //          <p><strong>Duration: </strong>{project.duration}</p>
-      //          <p><strong>Website: </strong>
-      //             <a href={project.website}>{project.website}</a>
-      //          </p>
-      //          <p><strong>Framework: </strong>{project.framework}</p>
-      //          <p><strong>Template: </strong>{project.template}</p>
-      //          <p><strong>Details: </strong>{project.details}</p>
-      //       </Col>
-      //       <Col {...spanM}>
-      //          <Image src={project.img} />
-      //          <a href={'/projects/' + project.id} className="ps-btn position-absolute bottom-0 start-50 translate-middle-x mb-5 d-block">View More</a>
-      //       </Col>
-      //    </Row>
-      // </div>
+      <Row style={{ padding: "250px 0 150px 0", backgroundColor: "#000000", minHeight: "calc(100vh - 346px)" }}>
+         <Col span={24} className="container">
+            <Row>
+               <div className="background-overlay">
+                  <ParticlesComponent />
+               </div>
+            </Row>
+            <Row>
+               {!project
+                  ? <ProjectNoExist />
+                  : <ProjectDetail />
+               }               
+            </Row>
+         </Col>
+      </Row>
    );
 }
 
